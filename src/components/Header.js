08b1a33fb9be48bd4d6b1d3ec12830d8a63385ba
deza395 <debora.ed.zarate@gmail.com>
2021-07-29 21:42:15 -0300

    modiciafiones en la portada

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
+import logo from "../mimiLogo.png";
 
 function Header() {
   return (
     <header>
       <div className="ToWrap">
-        <div className="main-menu"></div>
+        <div className="main-menu">
+          <div className="logo-center">
+            <img className="logo-main" src={logo} />
+          </div>
+          <div className="menu">
+            {" "}
+            <img className="image-top-menu" src="/nube1.png" />
+            <p className="description-menu">
+              {" "}
+              Somos una empresa familiar dedicada a brindar servicios para
+              fiestas infantiles. Ofrecemos distintos tipos de juegos inflables,
+              juego de salón y todo el equipamiento necesario para fiestas de
+              karaoke. Comunicate con nosotros para darle vida a tus fiestas
+            </p>
+            <img src="/nube2.png" />
+          </div>
+        </div>
         <div className="bottom-menu"></div>
       </div>
     </header>
